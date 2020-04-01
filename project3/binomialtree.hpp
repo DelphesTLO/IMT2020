bc140bf3ec5e1fbd5fa4925bb3134c673bb1814a
Lucas GAUTIER <lucas.gautier@imt-atlantique.net>
2020-04-01 19:20:54 +0200

    modification of underlying

--- project3/binomialtree.hpp
+++ project3/binomialtree.hpp
@@ -4,16 +4,13 @@
  Copyright (C) 2003 Ferdinando Ametrano
  Copyright (C) 2001, 2002, 2003 Sadruddin Rejeb
  Copyright (C) 2005 StatPro Italia srl
-
  This file is part of QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
-
  QuantLib is free software: you can redistribute it and/or modify it
  under the terms of the QuantLib license.  You should have received a
  copy of the license along with this program; if not, please email
  <quantlib-dev@lists.sf.net>. The license is also available online at
  <http://quantlib.org/license.shtml>.
-
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the license for more details.
@@ -36,23 +33,23 @@ namespace QuantLib {
     /*! \ingroup lattices */
     template <class T>
     class BinomialTree_2 : public Tree<T> {
-      public:
+    public:
         enum Branches { branches = 2 };
         BinomialTree_2(const boost::shared_ptr<StochasticProcess1D>& process,
-                       Time end,
-                       Size steps)
-        : Tree<T>(steps+1) {
+            Time end,
+            Size steps)
+            : Tree<T>(steps + 1) {
             x0_ = process->x0();
-            dt_ = end/steps;
+            dt_ = end / steps;
             driftPerStep_ = process->drift(0.0, x0_) * dt_;
         }
         Size size(Size i) const {
-            return i+1;
+            return i + 3;
         }
         Size descendant(Size, Size index, Size branch) const {
             return index + branch;
         }
-      protected:
+    protected:
         Real x0_, driftPerStep_;
         Time dt_;
     };
@@ -62,19 +59,18 @@ namespace QuantLib {
     /*! \ingroup lattices */
     template <class T>
     class EqualProbabilitiesBinomialTree_2 : public BinomialTree_2<T> {
-      public:
+    public:
         EqualProbabilitiesBinomialTree_2(
-                        const boost::shared_ptr<StochasticProcess1D>& process,
-                        Time end,
-                        Size steps)
-        : BinomialTree_2<T>(process, end, steps) {}
+            const boost::shared_ptr<StochasticProcess1D>& process,
+            Time end,
+            Size steps)
+            : BinomialTree_2<T>(process, end, steps) {}
         Real underlying(Size i, Size index) const {
-            BigInteger j = 2*BigInteger(index) - BigInteger(i);
-            // exploiting the forward value tree centering
-            return this->x0_*std::exp(i*this->driftPerStep_ + j*this->up_);
+            BigInteger j = 2 * BigInteger(index) - BigInteger(i) - BigInteger(2);
+            return this->x0_ * std::exp(i * this->driftPerStep_ + j * this->up_);
         }
         Real probability(Size, Size, Size) const { return 0.5; }
-      protected:
+    protected:
         Real up_;
     };
 
@@ -83,21 +79,20 @@ namespace QuantLib {
     /*! \ingroup lattices */
     template <class T>
     class EqualJumpsBinomialTree_2 : public BinomialTree_2<T> {
-      public:
+    public:
         EqualJumpsBinomialTree_2(
-                        const boost::shared_ptr<StochasticProcess1D>& process,
-                        Time end,
-                        Size steps)
-        : BinomialTree_2<T>(process, end, steps) {}
+            const boost::shared_ptr<StochasticProcess1D>& process,
+            Time end,
+            Size steps)
+            : BinomialTree_2<T>(process, end, steps) {}
         Real underlying(Size i, Size index) const {
-            BigInteger j = 2*BigInteger(index) - BigInteger(i);
-            // exploiting equal jump and the x0_ tree centering
-            return this->x0_*std::exp(j*this->dx_);
+            BigInteger j = 2 * BigInteger(index) - BigInteger(i) - BigInteger(2);
+            return this->x0_ * std::exp(j * this->dx_);
         }
         Real probability(Size, Size, Size branch) const {
             return (branch == 1 ? pu_ : pd_);
         }
-      protected:
+    protected:
         Real dx_, pu_, pd_;
     };
 
@@ -105,11 +100,11 @@ namespace QuantLib {
     //! Jarrow-Rudd (multiplicative) equal probabilities binomial tree
     /*! \ingroup lattices */
     class JarrowRudd_2 : public EqualProbabilitiesBinomialTree_2<JarrowRudd_2> {
-      public:
+    public:
         JarrowRudd_2(const boost::shared_ptr<StochasticProcess1D>&,
-                     Time end,
-                     Size steps,
-                     Real strike);
+            Time end,
+            Size steps,
+            Real strike);
     };
 
 
@@ -117,11 +112,11 @@ namespace QuantLib {
     /*! \ingroup lattices */
     class CoxRossRubinstein_2
         : public EqualJumpsBinomialTree_2<CoxRossRubinstein_2> {
-      public:
+    public:
         CoxRossRubinstein_2(const boost::shared_ptr<StochasticProcess1D>&,
-                            Time end,
-                            Size steps,
-                            Real strike);
+            Time end,
+            Size steps,
+            Real strike);
     };
 
 
@@ -129,85 +124,89 @@ namespace QuantLib {
     /*! \ingroup lattices */
     class AdditiveEQPBinomialTree_2
         : public EqualProbabilitiesBinomialTree_2<AdditiveEQPBinomialTree_2> {
-      public:
+    public:
         AdditiveEQPBinomialTree_2(
-                        const boost::shared_ptr<StochasticProcess1D>&,
-                        Time end,
-                        Size steps,
-                        Real strike);
+            const boost::shared_ptr<StochasticProcess1D>&,
+            Time end,
+            Size steps,
+            Real strike);
     };
 
 
     //! %Trigeorgis (additive equal jumps) binomial tree
     /*! \ingroup lattices */
     class Trigeorgis_2 : public EqualJumpsBinomialTree_2<Trigeorgis_2> {
-      public:
+    public:
         Trigeorgis_2(const boost::shared_ptr<StochasticProcess1D>&,
-                     Time end,
-                     Size steps,
-                     Real strike);
+            Time end,
+            Size steps,
+            Real strike);
     };
 
 
     //! %Tian tree: third moment matching, multiplicative approach
     /*! \ingroup lattices */
     class Tian_2 : public BinomialTree_2<Tian_2> {
-      public:
+    public:
         Tian_2(const boost::shared_ptr<StochasticProcess1D>&,
-               Time end,
-               Size steps,
-               Real strike);
+            Time end,
+            Size steps,
+            Real strike);
         Real underlying(Size i, Size index) const {
-            return x0_ * std::pow(down_, Real(BigInteger(i)-BigInteger(index)))
-                       * std::pow(up_, Real(index));
+            return x0_ * std::pow(down_, Real(BigInteger(i) - BigInteger(index)) + 1)
+                * std::pow(up_, Real(index) - 1);
+            //return x0_ * std::pow(down_, Real(BigInteger(i)-BigInteger(index)))
+            //         * std::pow(up_, Real(index) );
         };
         Real probability(Size, Size, Size branch) const {
             return (branch == 1 ? pu_ : pd_);
         }
-      protected:
+    protected:
         Real up_, down_, pu_, pd_;
     };
 
     //! Leisen & Reimer tree: multiplicative approach
     /*! \ingroup lattices */
     class LeisenReimer_2 : public BinomialTree_2<LeisenReimer_2> {
-      public:
+    public:
         LeisenReimer_2(const boost::shared_ptr<StochasticProcess1D>&,
-                       Time end,
-                       Size steps,
-                       Real strike);
+            Time end,
+            Size steps,
+            Real strike);
         Real underlying(Size i, Size index) const {
-            return x0_ * std::pow(down_, Real(BigInteger(i)-BigInteger(index)))
-                       * std::pow(up_, Real(index));
+            return x0_ * std::pow(down_, Real(BigInteger(i) - BigInteger(index)) + 1)
+                * std::pow(up_, Real(index) - 1);
+            //return x0_ * std::pow(down_, Real(BigInteger(i)-BigInteger(index)))
+            //          * std::pow(up_, Real(index));
         }
         Real probability(Size, Size, Size branch) const {
             return (branch == 1 ? pu_ : pd_);
         }
-      protected:
+    protected:
         Real up_, down_, pu_, pd_;
     };
 
 
-     class Joshi4_2 : public BinomialTree_2<Joshi4_2> {
-      public:
+
+    class Joshi4_2 : public BinomialTree_2<Joshi4_2> {
+    public:
         Joshi4_2(const boost::shared_ptr<StochasticProcess1D>&,
-                 Time end,
-                 Size steps,
-                 Real strike);
+            Time end,
+            Size steps,
+            Real strike);
         Real underlying(Size i, Size index) const {
-            return x0_ * std::pow(down_, Real(BigInteger(i)-BigInteger(index)))
-                       * std::pow(up_, Real(index));
+            return x0_ * std::pow(down_, Real(BigInteger(i) - BigInteger(index)) + 1)
+                * std::pow(up_, Real(index) - 1);
+            //return x0_ * std::pow(down_, Real(BigInteger(i)-BigInteger(index)))
+            //           * std::pow(up_, Real(index));
         }
         Real probability(Size, Size, Size branch) const {
             return (branch == 1 ? pu_ : pd_);
         }
-      protected:
+    protected:
         Real computeUpProb(Real k, Real dj) const;
         Real up_, down_, pu_, pd_;
     };
 
-
 }
-
-
 #endif
